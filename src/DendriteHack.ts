df1e2c8499bcbf387422949c47491cd169277b61
Firepup650 <70233190+Firepup6500@users.noreply.github.com>
2024-04-29 16:56:23 -0500

    Actually add the function

--- src/DendriteHack.ts
+++ src/DendriteHack.ts
@@ -1,1 +1,11 @@
-// TODO
+/**
+  * Manipulates nicks into a state that dendrite will handle
+  * @param nick The nick to change
+  * @return A string that is the nick fixed for dendrite
+*/
+export function DendriteHack(nick: string): string {
+  string newNick = nick.toLowerCase();
+  // TODO: Handle bad symbols here
+
+  return newNick;
+}
